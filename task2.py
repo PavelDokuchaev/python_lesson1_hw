75d81ba703a0a9feb3453e1457a1232bdc75fe81
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-11-29 17:57:13 +0400

    task2_ver_2

--- task2.py
+++ task2.py
@@ -5,7 +5,4 @@ time = int(input("Введите время в секундах "))
 hours = time // 3600
 minutes = (time - hours * 3600) // 60
 seconds = time - (hours * 3600 + minutes * 60)
-print(hours)
-print(minutes)
-print(seconds)
 print(f"Время в формате чч:мм:сс   {hours} : {minutes} : {seconds}")
