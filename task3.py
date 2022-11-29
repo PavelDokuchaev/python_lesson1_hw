62afde8d50b78400fc27dd6acfc8f19eed7afb7d
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-11-29 17:52:57 +0400

    lesson_1_home_work_ver_2

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 """ Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
 Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369. """
 
-user_number = int(input('Введите число: '))
-nn = 10 * user_number + user_number
-nnn = 100 * user_number + nn
-print(user_number + nn + nnn)
+n = int(input('Введите число: '))
+nn = 10 * n + n
+nnn = 100 * n + nn
+print(f'Сумма чисел n + nn + nnn равна: {nnn}')
