559496497b55317c5de62035b593ed38f1aaac81
dokuchaev.pa <dokuchaev.pa@gmail.com>
2022-12-14 19:19:38 +0400

    sem5_task4

--- seminar5_task4.py
+++ seminar5_task4.py
@@ -7,4 +7,12 @@ Four — 4
 построчно данные. При этом английские числительные должны заменяться
 на русские. Новый блок строк должен записываться в новый текстовый файл."""
 
-
+rus = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_file = []
+with open('numbers.txt', 'r') as f_obj:
+    for line in f_obj:
+        line = line.split(' ', 1)
+        new_file.append(rus[line[0]] + ' ' + line[1])
+    print(new_file)
+with open('numbers_new.txt', 'w') as file_obj_2:
+    file_obj_2.writelines(new_file)
